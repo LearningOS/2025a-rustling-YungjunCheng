5c617e3cbe1d72c1b366439c0845787cf89f1ad1
Cheng_Y0ngjun <2544314512@qq.com>
2025-10-05 22:39:47 +0800

    complete rustlings exercises

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -9,10 +9,8 @@
 // Execute `rustlings hint clippy1` or use the `hint` watch subcommand for a
 // hint.
 
-use std::f32;
-
 fn main() {
-    let pi = f32::consts::PI;
+    let pi = std::f32::consts::PI;
     let radius = 5.00f32;
 
     let area = pi * f32::powi(radius, 2);
